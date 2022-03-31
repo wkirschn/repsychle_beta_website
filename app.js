d299d690368cc43ee00e934ff2108d4f43ddfb09
Wyatt <wkirschn@uoguelph.ca>
2022-03-30 22:52:10 -0400

    Testing to see what appears on heroku

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-
-
-if (process.env.NODE_ENV != 'production') {
-  require('dotenv').config();
-}
-
-
 var createError = require('http-errors');
-
-
-
-
-
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -20,21 +8,83 @@ const indexRouter = require('./routes/index');
 
 const testRouter = require('./routes/test');
 
-const loginRouter = require('./routes/login_t')
+const loginRouter = require('./routes/login_t');
+const mongoose = require('mongoose');
+
+
+
+var app = express();
+
+
+/*
+/!*Mongo DB*!/
+
+const morgan = require('morgan')
+
+const dbURI = process.env.DATABASE_URL;
+
+// Connect to MongoDB
+
+*/
+
+
+
+
+
+
+/*const {MongoClient} = require("mongodb");*/
 
 /*
   Implementing MongoDB
  */
 
-const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL).then(r => console.error(r));
+if(process.env.NODE_ENV !== 'production') {
+require('dotenv').config()
+}
+
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+db.once('open', () => console.log('Connected'))
 
 
-var app = express();
+
+
+
+
+
+/*
+async function main() {
+  const uri = "mongodb+srv://repsychle_production:thisisatestingpassword@cluster0.vtfzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    await listDatabases(client);
+
+
+  }
+
+  catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+main().catch(console.error);
+
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases: ");
+  databasesList.databases.forEach(db => {
+    console.log('- ${db.name}');
+  })
+}
+*/
+
 
 app.use('/favicon.ico', express.static('./public/images/'));
 
@@ -56,7 +106,7 @@ app.use('/login', loginRouter);
 app.use("/public/", express.static('public'));
 
 
-
+app.listen(process.env.PORT || 4000)
 
 
 // catch 404 and forward to error handler
